6d4cba57c849d7d57475283d99ea46c7c7643b4b
suicidalbombzzz <74739277+suicidalbombzzz@users.noreply.github.com>
2020-11-19 20:30:44 +0000

    Add files via upload

---
+++ Pedo bot.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def whoarethepedos(ctx):
+    await ctx.send('@Shadowgamer12168#7783')
+
+
+bot.run('Nzc5MDY1ODI4OTM4NjEyNzQ3.X7bHlw.vxNnV2OlZBV6rsFUh5F6ZyOXvLw')
